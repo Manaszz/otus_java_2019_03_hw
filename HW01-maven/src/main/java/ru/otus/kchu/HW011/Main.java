ca97aadc03a246f42a2e144d35ef80b06bd7101a
Manaszz <konst.c@pm.me>
2019-05-05 14:03:51 +0300

    1st

---
+++ HW01-maven/src/main/java/ru/otus/kchu/HW011/Main.java
@@ -0,0 +1,14 @@
+package ru.otus.kchu.HW011;
+
+import static com.google.common.base.Objects.equal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello otus!");
+    int a= 5,b = 8;
+     System.out.println(equal(a,b));
+
+    }
+}
